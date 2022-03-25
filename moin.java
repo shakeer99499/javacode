70c2f1baa4a09fe53f8eaea75aefed5d7636d70f
shakeer99499 <shakeer99499@gmail.com>
2022-03-26 03:42:33 +0530

    first commit

---
+++ moin.java
@@ -0,0 +1,6 @@
+public class moin {
+    public static void main(String []args) {
+        System.out.print("Studyopedia Java 17 Tutorial") ;
+        System.out.print("Our First Java 17 Program") ;
+    }
+}
